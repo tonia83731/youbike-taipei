cdcbef96f616b1118944ec8312e84ad4dcdc6b11
tonia94731 <huiyu.tonia.chang@gmail.com>
2024-02-26 17:28:16 +0800

    feat: add news page and contact page

---
+++ pages/contact/index.jsx
@@ -0,0 +1,12 @@
+import HeadSettings from "@/components/head/HeadSettings";
+export default function NewsPage() {
+  return (
+    <>
+      <HeadSettings
+        pageName="聯絡我們"
+        pageDescription="This is YouBike contact page."
+      />
+      <section className=""></section>
+    </>
+  );
+}
---
+++ pages/news/index.jsx
@@ -0,0 +1,15 @@
+import HeadSettings from "@/components/head/HeadSettings"
+export default function NewsPage () {
+  return (
+    <>
+      <HeadSettings
+        pageName="最新消息"
+        pageDescription="This is YouBike news page."
+      />
+      <section className="">
+
+      </section>
+    </>
+  );
+}
+
