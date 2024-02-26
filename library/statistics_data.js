fd7a77e220fb809afc81f7c3ab4ed32970441d6f
tonia94731 <huiyu.tonia.chang@gmail.com>
2024-02-26 22:35:28 +0800

    feat: update node version to 20.11.1

--- library/statistics_data.js
+++ library/statistics_data.js
@@ -1,23 +1,30 @@
-
+const baseUrl = process.env.NEXT_PUBLIC_STATISTICS_URL;
+// console.log(baseUrl)
 
 export const getPopularStopData = async () => {
   try {
     const res = await fetch(
-      "https://data.taipei/api/v1/dataset/c7a076dd-8aa6-4129-9c8a-e04cbf18694f?scope=resourceAquire&limit=50"
+      `${baseUrl}/c7a076dd-8aa6-4129-9c8a-e04cbf18694f?scope=resourceAquire`
     );
-    const data = res.json()
+    // console.log(res)
+    const data = await res.json()
     return data
   } catch (error) {
     console.error('[Get popular stop data failed]:', error)
+    return null
   }
 } 
 
 export const getMonthlyUseData = async () => {
   try {
-    const res = await fetch ("https://data.taipei/api/v1/dataset/8f690548-61bc-4bff-8baa-01d465eb672c?scope=resourceAquire&limit=50")
-    const data = res.json()
+    const res = await fetch(
+      `${baseUrl}/8f690548-61bc-4bff-8baa-01d465eb672c?scope=resourceAquire&limit=50`
+    );
+    const data = await res.json()
+    // console.log(data)
     return data
   } catch (error) {
     console.error('[Get Monthy user data failed]:', error)
+    return null
   }
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  server: {
+    host: '127.0.0.1'
+  }
 };
 
 export default nextConfig;
