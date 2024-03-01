b307ca888766ba2b6a43b01818b1f2146ee83543
tonia94731 <huiyu.tonia.chang@gmail.com>
2024-03-01 13:55:10 +0800

    style: edit backstage/news-list form to be fixed

--- components/backstage/BackNewsTable.jsx
+++ components/backstage/BackNewsTable.jsx
@@ -1,98 +1,124 @@
 import dayjs from "dayjs";
+import Pagination from "../realtime/Pagination";
+import { useState } from "react";
 
 export default function BackNewsTable(props) {
-  const { newsList, onNewsEdit } = props;
+  const { newsList, onNewsEdit, onNewsDelete } = props;
+  const [currentPage, setCurrentPage] = useState(1)
   // console.log(newsList)
   const theadData = ["活動時間", "活動標題", "活動內文", "更新時間", "其他"];
-  const handleNewsDelete = async (_id) => {
-    try {
-      const response = await fetch("/api/news", {
-        method: "DELETE",
-        body: JSON.stringify({ id: _id }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      // console.log(response)
-      if(response.ok) {
-        const data = await response.json()
-        // console.log(data)
-        const { message } = data;
-        console.log(message);
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+
+  const perPage = 10
+  const length = newsList?.length
+  const show_page = 5;
+  const numPage = Math.ceil(length / perPage);
+  const numArray = Array.from({ length: numPage }, (_, index) => index + 1);
+  const showNumArray =
+    currentPage < 3
+      ? numArray.slice(0, 5)
+      : currentPage > numPage - 2
+      ? numArray.slice(numPage - show_page, numPage)
+      : numArray.slice(currentPage - 3, currentPage + 2);
+  
+  const firstIndex = (currentPage - 1) * perPage;
+  const lastIndex = currentPage * perPage;
+  const showList = newsList.slice(firstIndex, lastIndex)
+  const handlePageClick = (event) => {
+    const id = event.target.closest("button").id;
+    if (currentPage > 1 && id === "first-btn") setCurrentPage(1);
+    else if (currentPage > 1 && id === "prev-btn")
+      setCurrentPage(currentPage - 1);
+    else if (currentPage < numPage && id === "last-btn")
+      setCurrentPage(numPage);
+    else if (currentPage < numPage && id === "next-btn")
+      setCurrentPage(currentPage + 1);
+    else setCurrentPage(+id);
+  };
+
   return (
-    <table className="w-full mb-6">
-      <thead>
-        <tr className="bg-lime-100 text-white font-bold">
-          {theadData.map((data, index) => {
-            // console.log(index)
+    <>
+      <table className="w-full mb-6">
+        <thead>
+          <tr className="bg-lime-100 text-white font-bold">
+            {theadData.map((data, index) => {
+              // console.log(index)
+              return (
+                <th
+                  className={`h-12 ${
+                    index === 2 || index === 3 ? "hidden md:table-cell" : ""
+                  }`}
+                  key={data}
+                >
+                  {data}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody className="text-xl">
+          {showList.map((data, index) => {
+            // console.log(data);
+            const {
+              _id,
+              title,
+              subtitle,
+              description,
+              startDate,
+              endDate,
+              updateAt,
+            } = data;
+
+            const startAt = dayjs(startDate).format("YYYY/MM/DD");
+            const endAt = dayjs(endDate).format("YYYY/MM/DD");
+            const updateAt_date = dayjs(updateAt).format("YYYY/MM/DD");
+            const updateAt_time = dayjs(updateAt).format("HH:mm:ss");
             return (
-              <th
-                className={`h-12 ${
-                  index === 2 || index === 3 ? "hidden md:table-cell" : ""
+              <tr
+                className={`h-16 ${
+                  index % 2 === 0 ? "bg-white " : "bg-slate-200"
                 }`}
-                key={data}
+                key={_id}
               >
-                {data}
-              </th>
+                <td className="px-2 text-center text-olive-100">
+                  {startAt}
+                  <br />~{endAt}
+                </td>
+                <td className="px-2 text-olive-100">
+                  <div className="font-bold text-xl">{title}</div>
+                  <div className="text-lg">{subtitle}</div>
+                </td>
+                <td className="w-60 hidden px-2 text-olive-100 md:table-cell">
+                  {description}
+                </td>
+                <td className="hidden px-2 text-center text-olive-100 md:table-cell">
+                  {updateAt_date} <br />
+                  {updateAt_time}
+                </td>
+                <td className="w-28 px-2 text-center text-olive-100 flex flex-col justify-center my-2">
+                  <button
+                    className="py-2 bg-lemon-100 text-white rounded-md"
+                    onClick={() => onNewsEdit(_id)}
+                  >
+                    修改
+                  </button>
+                  <button
+                    className="py-2 bg-mustard-100 text-white rounded-md mt-1"
+                    onClick={() => onNewsDelete(_id)}
+                  >
+                    刪除
+                  </button>
+                </td>
+              </tr>
             );
           })}
-        </tr>
-      </thead>
-      <tbody className="text-xl">
-        {newsList.map((data, index) => {
-          // console.log(data);
-          const {
-            _id,
-            title,
-            subtitle,
-            description,
-            startDate,
-            endDate,
-            updateAt,
-          } = data;
-
-          const startAt = dayjs(startDate).format("YYYY/MM/DD");
-          const endAt = dayjs(endDate).format("YYYY/MM/DD");
-          const updateAt_date = dayjs(updateAt).format("YYYY/MM/DD");
-          const updateAt_time = dayjs(updateAt).format("HH:mm:ss");
-          return (
-            <tr
-              className={`h-16 ${
-                index % 2 === 0 ? "bg-white " : "bg-slate-200"
-              }`}
-              key={_id}
-            >
-              <td className="px-2 text-center text-olive-100">
-                {startAt}
-                <br />~{endAt}
-              </td>
-              <td className="px-2 text-olive-100">
-                <div className="font-bold text-xl">{title}</div>
-                <div className="text-lg">{subtitle}</div>
-              </td>
-              <td className="hidden px-2 text-olive-100 md:table-cell">
-                {description}
-              </td>
-              <td className="hidden px-2 text-center text-olive-100 md:table-cell">
-                {updateAt_date} <br/>{updateAt_time}
-              </td>
-              <td className="px-2 text-center text-olive-100 flex flex-col justify-center my-2">
-                <button className="py-2 bg-lemon-100 text-white rounded-md" onClick={() => onNewsEdit(_id)}>
-                  修改
-                </button>
-                <button className="py-2 bg-mustard-100 text-white rounded-md mt-1" onClick={() => handleNewsDelete(_id)}>
-                  刪除
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+      <Pagination
+        pages={showNumArray}
+        currentPage={currentPage}
+        totalPage={numPage}
+        onPageClick={handlePageClick}
+      />
+    </>
   );
 }
--- pages/backstage/news-list.jsx
+++ pages/backstage/news-list.jsx
@@ -16,6 +16,31 @@ export default function NewsListPage() {
     setEditData([newsData]);
     // updateNewsData(newsData);
   }
+
+  const handleNewsDelete = async (_id) => {
+    try {
+      const response = await fetch("/api/news", {
+        method: "DELETE",
+        body: JSON.stringify({ id: _id }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // console.log(response)
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data)
+        const filterNewsList = newsList.filter((data) => data._id 
+        !== _id)
+        setNewsList(filterNewsList)
+
+        const { message } = data;
+        console.log(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   // const updateNewsData = (data) => {
   //   setNewsData(data);
   // };
@@ -35,23 +60,36 @@ export default function NewsListPage() {
   //  };
 
   useEffect(() => {
-    fetch("/api/news")
-      .then((response) => response.json())
-      .then((data) => {
-        const { news } = data;
-        // console.log(news)
-        setNewsList(news);
-      });
+    const getNewsDataAsync = async () => {
+      try {
+        const response = await fetch("/api/news");
+        if(response.ok) {
+          const data = await response.json()
+          const {news} = data
+          // console.log(news)
+          setNewsList(news);
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getNewsDataAsync()
   }, []);
 
   return (
     <BackStageLayout pageName="消息列表">
-      <div className="lg:grid lg:grid-cols-3 lg:gap-2 lg:mb-6">
-        <div className="">
-          <BackNewsForm editData={editData} />
+      <div className="xl:grid xl:grid-cols-3 xl:gap-2 xl:mb-6">
+        <div className="relative h-full w-full">
+          <div className="xl:fixed xl:w-[29%] xl:max-w-[420px]">
+            <BackNewsForm editData={editData} />
+          </div>
         </div>
-        <div className="mt-4 lg:mt-0 lg:col-span-2">
-          <BackNewsTable newsList={newsList} onNewsEdit={handleNewsEdit} />
+        <div className="mt-4 xl:mt-0 xl:col-span-2">
+          <BackNewsTable
+            newsList={newsList}
+            onNewsEdit={handleNewsEdit}
+            onNewsDelete={handleNewsDelete}
+          />
         </div>
       </div>
     </BackStageLayout>
