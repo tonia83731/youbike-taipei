da0fb741f15656972c4817a069677ea51ccfee57
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-03-06 14:25:04 +0800

    fix: remove authlayout in adim/login, add useSession the check authentication

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -6,13 +6,16 @@ import { useState } from "react";
 
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 export default function BackStageLogin() {
   const router = useRouter();
+  const { data: session, status } = useSession();
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
+  console.log(status);
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
@@ -27,47 +30,47 @@ export default function BackStageLogin() {
     }
   };
 
+  if (status === "authenticated") router.replace("/backstage/news-list");
+
   return (
     <>
-      <AuthLayout>
-        <HeadSettings
-          pageName="後臺登入"
-          pageDescription="This is YouBike news page."
-        />
-        <section className="mb-6">
-          <h3 className="font-bold text-2xl text-center mb-6 text-olive-100">
-            後臺登入
-          </h3>
-          <form action="" className="" onSubmit={handleLoginSubmit}>
-            <DefaultInput
-              label="Email"
-              id="backLogin-email"
-              name="email"
-              isRequired={true}
-              inputValue={loginData.email}
-              onInputChange={(e) =>
-                setLoginData({ ...loginData, email: e.target.value })
-              }
-            />
-            <DefaultInput
-              label="Password"
-              id="backLogin-password"
-              name="password"
-              isRequired={true}
-              inputValue={loginData.password}
-              onInputChange={(e) =>
-                setLoginData({ ...loginData, password: e.target.value })
-              }
-            />
-            <button
-              type="submit"
-              className="w-full py-2 mt-4 bg-lime-100 text-white rounded-md"
-            >
-              登入
-            </button>
-          </form>
-        </section>
-      </AuthLayout>
+      <HeadSettings
+        pageName="後臺登入"
+        pageDescription="This is YouBike news page."
+      />
+      <section className="mb-6">
+        <h3 className="font-bold text-2xl text-center mb-6 text-olive-100">
+          後臺登入
+        </h3>
+        <form action="" className="" onSubmit={handleLoginSubmit}>
+          <DefaultInput
+            label="Email"
+            id="backLogin-email"
+            name="email"
+            isRequired={true}
+            inputValue={loginData.email}
+            onInputChange={(e) =>
+              setLoginData({ ...loginData, email: e.target.value })
+            }
+          />
+          <DefaultInput
+            label="Password"
+            id="backLogin-password"
+            name="password"
+            isRequired={true}
+            inputValue={loginData.password}
+            onInputChange={(e) =>
+              setLoginData({ ...loginData, password: e.target.value })
+            }
+          />
+          <button
+            type="submit"
+            className="w-full py-2 mt-4 bg-lime-100 text-white rounded-md"
+          >
+            登入
+          </button>
+        </form>
+      </section>
     </>
   );
 }
