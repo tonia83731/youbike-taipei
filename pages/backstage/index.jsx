a110a53df48272e64efe6de0981d031bda1e851f
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-03-21 16:58:10 +0800

    fix: fixed no router instance error in backstage index.jsx

--- pages/backstage/index.jsx
+++ pages/backstage/index.jsx
@@ -1,9 +1,18 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Loading from "@/components/loading/Loading";
 
 export default function BackstageHome() {
   const router = useRouter();
   const { data: session, status } = useSession();
-  if (status === "authenticated") router.replace("/backstage/news-list");
-  else router.replace("/admin/login");
+  useEffect(() => {
+    if (status === "authenticated") router.replace("/backstage/news-list");
+    else router.replace("/admin/login");
+  }, [status, router]);
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
 }
