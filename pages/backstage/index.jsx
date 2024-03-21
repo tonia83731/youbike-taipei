2f6dbc4186eedd5f1c3b870b9a32f6141cf9941a
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-03-21 17:03:06 +0800

    feat: remove next/router in backstage index.jsx

--- pages/backstage/index.jsx
+++ pages/backstage/index.jsx
@@ -1,15 +1,16 @@
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Loading from "@/components/loading/Loading";
 
 export default function BackstageHome() {
-  const router = useRouter();
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   useEffect(() => {
-    if (status === "authenticated") router.replace("/backstage/news-list");
-    else router.replace("/admin/login");
-  }, [status, router]);
+    if (status === "authenticated") {
+      window.location.href = "/backstage/news-list";
+    } else {
+      window.location.href = "/admin/login";
+    }
+  }, [status]);
   return (
     <div>
       <Loading />
